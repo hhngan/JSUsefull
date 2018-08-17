6cfd67e371a2479c45a2481b8e33db28c597d0c2
Ngan (Brian) Huynh <hhngan.agu@gmail.com>
2018-08-17 08:48:40 +0700

    initial version

---
+++ stringFunction.js
@@ -0,0 +1,7 @@
+function pad_with_zeroes(number, length) {
+    var my_string = '' + number;
+    while (my_string.length < length) {
+        my_string = '0' + my_string;
+    }    
+    return my_string;    
+}
