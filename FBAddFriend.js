704a9e6c65fc9016cb0e00c28ebe3b3c5ccfaba8
Ngan (Brian) Huynh <hhngan.agu@gmail.com>
2018-11-12 10:14:28 +0700

    Create FBAddFriend.js

---
+++ FBAddFriend.js
@@ -0,0 +1,3 @@
+var intervalID = setInterval(function () {
+var inputs = document.getElementsByClassName('_42ft _4jy0 FriendRequestAdd addButton _4jy3 _4jy1 selected _51sy');for(var i=0; i<inputs.length;i++) { inputs[i].click();8 }
+}, 3000);
