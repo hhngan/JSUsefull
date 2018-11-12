e11930c98e48a1e79038bb3b9af78af65c2f0706
Ngan (Brian) Huynh <hhngan.agu@gmail.com>
2018-11-12 10:13:16 +0700

    Create FB_Friends_remove.js

---
+++ FB_Friends_remove.js
@@ -0,0 +1,6 @@
+#I was tired to see facebook friend suggestion on facebook so i tried to remove all suggested friends in mass :
+
+var intervalID = setInterval(function () {
+
+javascript:var inputs = document.getElementsByClassName("_42ft _4jy0 _3-99 _4jy3 _517h _51sy"); for(var i=0; i<inputs.length;i++) { inputs[i].click();8 }
+}, 100);
