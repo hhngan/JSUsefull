55dcd7695d261c333a889e5963ce999b6f370eaa
Ngan (Brian) Huynh <hhngan.agu@gmail.com>
2018-11-14 13:14:59 +0700

    Update FBAutoLike.js

--- FBAutoLike.js
+++ FBAutoLike.js
@@ -1,10 +1,10 @@
 setInterval(function () {	
-	var inputs = document.getElementsByClassName('UFILikeLink _4x9- _4x9_ _48-k'); //UFILikeLink _4x9- _4x9_ _48-k UFILinkBright
+	var inputs = document.getElementsByClassName('UFILikeLink _4x9- _4x9_ _48-k');
 	for(var i=0; i<inputs.length;i++) { 
-		var eleInput = inputs[i].className.includes('UFILinkBright');		
-		if(eleInput){		
-			console.log('Yes');
-			eleInput[i].click(); 
+		var eleInput = inputs[i].className.includes('UFILinkBright');
+		if(!eleInput){		
+			console.log('LikeClick');
+			inputs[i].click(); 
 		}
 		else 
 			console.log('No');
